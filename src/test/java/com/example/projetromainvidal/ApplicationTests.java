18af0c326f40c55bb99e3ec7208b84c667c91c37
Romain Vidal <romain1.vidal@epita.fr>
2025-11-20 14:18:23 +0100

    first commit

---
+++ src/test/java/com/example/projetromainvidal/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetromainvidal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
